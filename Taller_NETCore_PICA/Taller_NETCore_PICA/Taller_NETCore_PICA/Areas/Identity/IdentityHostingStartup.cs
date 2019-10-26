a392f93e306b44fb5c1b829769d488c1b398b302
cian <diarmo2@hotmail.com>
2019-10-25 19:19:59 -0500

    First Commit

---
+++ Taller_NETCore_PICA/Taller_NETCore_PICA/Taller_NETCore_PICA/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taller_NETCore_PICA.Data;
+
+[assembly: HostingStartup(typeof(Taller_NETCore_PICA.Areas.Identity.IdentityHostingStartup))]
+namespace Taller_NETCore_PICA.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
